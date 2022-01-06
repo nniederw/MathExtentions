5db5845f0118dc63cfc57b67436205d0991ba1ed
nn <niels.niederwieser@gmx.ch>
2022-01-06 15:22:25 +0100

    update

--- MatrixCalc/MatrixCalc/Matrix.cs
+++ MatrixCalc/MatrixCalc/Matrix.cs
@@ -3,28 +3,27 @@ namespace MatrixCalc
 {
     public class Matrix<T> : IEquatable<Matrix<T>>
     {
-        public readonly int Columns;
         public readonly int Rows;
-        public (int, int) Dimension => new(Columns, Rows);
+        public readonly int Columns;
+        public (int, int) Dimension => new(Rows, Columns);
         private T[,] Values;
-        public Matrix(int columns, int rows)
+        private Func<T, T, T> Add;
+        private Func<T, T, T> Multiply;
+        public Matrix(int rows, int columns, Func<T, T, T> add, Func<T, T, T> multiply)
         {
             if (columns <= 0 || rows <= 0) { throw new Exception($"Columns and Rows can't be 0 or negative. Input: columns = {columns}; rows = {rows}"); }
             Columns = columns;
             Rows = rows;
-            Values = new T[Columns, Rows];
-            TestTOperations<T>();
+            Values = new T[Rows, Columns];
+            Add = add;
+            Multiply = multiply;
         }
-        public Matrix(int columns, int rows, T[,] values) : this(columns, rows)
+        public Matrix(T[,] values, Func<T, T, T> add, Func<T, T, T> multiply) : this(values.GetLength(0), values.GetLength(1), add, multiply)
         {
-            if (values.GetLength(0) != columns || values.GetLength(1) != rows)
+            var res = new T[Rows, Columns];
+            for (int i = 0; i < Rows; i++)
             {
-                throw new Exception("The length of the input array needs to be the same size as the matrix");
-            }
-            var res = new T[columns, rows];
-            for (int i = 0; i < Columns; i++)
-            {
-                for (int j = 0; j < Rows; j++)
+                for (int j = 0; j < Columns; j++)
                 {
                     res[i, j] = values[i, j];
                 }
@@ -35,36 +34,34 @@ namespace MatrixCalc
         {
             if (left.Columns != right.Columns || left.Rows != right.Rows)
             { throw new Exception($"For adding two matrices, they need to have the same dimensions. Input dimensions: left = {left.Columns}, {left.Rows}; right = {right.Columns}, {right.Rows}"); }
-            var result = new Matrix<T>(left.Columns, left.Rows);
-            for (int i = 0; i < left.Columns; i++)
+            var result = new Matrix<T>(left.Rows, left.Columns, left.Add, left.Multiply);
+            for (int i = 0; i < left.Rows; i++)
             {
-                for (int j = 0; j < left.Rows; j++)
+                for (int j = 0; j < left.Columns; j++)
                 {
-                    result.Values[i, j] = Plus(left.Values[i, j], right.Values[i, j]);
+                    result.Values[i, j] = left.Add(left.Values[i, j], right.Values[i, j]);
                 }
             }
             return result;
         }
         public static Matrix<T> operator *(Matrix<T> left, Matrix<T> right)
         {
-            if (left.Columns != right.Columns || left.Rows != right.Rows)
-            { throw new Exception($"For multipling two matrices, the rows of the left one needs to be the same as the columns of the right one. Input dimensions: left = {left.Columns}, {left.Rows}; right = {right.Columns}, {right.Rows}"); }
-            var result = new Matrix<T>(left.Rows, right.Columns);
+            if (left.Columns != right.Rows)
+            { throw new Exception($"For multipling two matrices, the columns of the left one needs to be the same as the rows of the right one. Input dimensions: left = {left.Columns}, {left.Rows}; right = {right.Columns}, {right.Rows}"); }
+            var result = new Matrix<T>(left.Rows, right.Columns, left.Add, left.Multiply);
             for (int column = 0; column < result.Columns; column++)
             {
                 for (int row = 0; row < result.Rows; row++)
                 {
-                    result.Values[column, row] = Sum(column, row);
+                    result.Values[row, column] = Sum(row, column);
                 }
             }
-            T Sum(int column, int row)
+            T Sum(int row, int column)
             {
                 T res = default;
-                int i = 0;
-                for (int j = 0; j < right.Columns && i < left.Rows; j++)
+                for (int i = 0, j = 0; i < left.Columns; j++, i++)
                 {
-                    res = Plus(res, Multiply(left.Values[column, i], right.Values[j, row]));
-                    i++;
+                    res = left.Add(res, left.Multiply(left.Values[row, i], right.Values[j, column]));
                 }
                 return res;
             }
@@ -100,38 +97,10 @@ namespace MatrixCalc
             }
             return false;
         }
-        private static void TestTOperations<Type>()
-        {
-            try
-            {
-                Type test1 = default;
-                Type test2 = default;
-                dynamic test1d = test1;
-                dynamic test2d = test2;
-                Type result = test1d + test2d;
-                result = test1d * test2d;
-            }
-            catch
-            {
-                throw new Exception($"The Type {nameof(Type)} can't be used for matrices");
-            }
-        }
-        private static T Plus(T item1, T item2)
-        {
-            dynamic d1 = item1;
-            dynamic d2 = item2;
-            return d1 + d2;
-        }
-        private static T Multiply(T item1, T item2)
-        {
-            dynamic d1 = item1;
-            dynamic d2 = item2;
-            return d1 * d2;
-        }
         public override string ToString()
         {
             string res = "{";
-            for (int i = 0; i < Columns; i++)
+            for (int i = 0; i < Rows; i++)
             {
                 if (i != 0)
                 {
@@ -142,7 +111,7 @@ namespace MatrixCalc
                     res += "{";
                 }
                 res = $"{res}{Values[i, 0]}";
-                for (int j = 1; j < Rows; j++)
+                for (int j = 1; j < Columns; j++)
                 {
                     res = $"{res}, {Values[i, j]}";
                 }
--- MatrixCalc/MatrixCalc/Program.cs
+++ MatrixCalc/MatrixCalc/Program.cs
@@ -5,32 +5,7 @@ namespace MatrixCalc
     {
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                if (count % 1000 == 0)
-                {
-                    Console.WriteLine(count);
-                }
-                Implication(GetRandomMatrix(), GetRandomMatrix());
-                count++;
-            }
-        }
-        private static Matrix<int> GetRandomMatrix()
-        {
-            Random random = new Random();
-            var ints = new int[,] { { random.Next(0, 10), random.Next(0, 10) }, { random.Next(0, 10), random.Next(0, 10) } };
-            return new Matrix<int>(2, 2, ints);
-        }
-        private static void Implication(Matrix<int> a, Matrix<int> b)
-        {
-            if (a * b != b * a)
-            {
-                if (a * a * b == a * b * a)
-                {
-                    Console.WriteLine($"Works: a = {a} b = {b}, ab = {a * b}, ba = {b * a}, aab = {a * a * b}, aba = {a * b * a}");
-                }
-            }
+            var im = MatrixType.Int(2, 2);
         }
     }
 }
