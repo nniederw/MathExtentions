5959fbb3bdf2b75ac4bf71cce0d8e51cf261d3fd
nniederw <niels.niederwieser@gmx.ch>
2022-02-19 16:05:38 +0100

    update

--- MathExtentions/MathExtentions/Program.cs
+++ MathExtentions/MathExtentions/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        // var im = MatrixType.Int(2, 2);
+        for (int modulo = 3; modulo < 21; modulo++)
+        {
+            Console.WriteLine($"modulo {modulo}");
+            for (int i = 2; i < modulo; i++)
+            {
+                Console.Write($"{new ModuloNumber(modulo,i).MultInverse()} ");
+            }
+            Console.WriteLine();
+        }
+        var m = new ModuloNumber(5,1);
     }
 }
